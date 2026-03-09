4c2d29e6bca2a3f3773e4454b86351ef71fe65a0
Bikram Pal <palbikram557@gmail.com>
2026-03-09 20:54:33 +0530

    add new problem car constructor

---
+++ src/Introduction/problem1.java
@@ -0,0 +1,13 @@
+package Introduction;
+class car {
+   car(){
+       System.out.println("car1");
+    }
+}
+
+public class problem1 {
+    public static void main(String[] args) {
+    car c1 = new car();
+
+    }
+}
